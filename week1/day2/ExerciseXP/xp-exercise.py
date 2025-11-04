a3c9446e7e9adbf937879538f38df70c09d6fb10
keltoum <keltoumidhssou1710@gmail.com>
2025-11-04 11:41:03 +0100

    remove the comments in xp-exercices and delete a temp file

--- week1/day2/ExerciseXP/xp-exercise.py
+++ week1/day2/ExerciseXP/xp-exercise.py
@@ -50,63 +50,63 @@ for number in range(1,21):
 
 #Exercise 6: While Loop
 print('**** exercise 6 ****')
-# name=input('enter your name : ')
-# while True:
+name=input('enter your name : ')
+while True:
 
-#     if len(name)<3:
-#         name=input('give correct name for your name (at least 3 characters long) : ')
-#         continue
-#     elif name.isdigit():
-#         name=input('give correct name for your name (no numbers) : ')
-#         continue
-#     else:
-#         print(f'thank you {name}')
-#         break
+    if len(name)<3:
+        name=input('give correct name for your name (at least 3 characters long) : ')
+        continue
+    elif name.isdigit():
+        name=input('give correct name for your name (no numbers) : ')
+        continue
+    else:
+        print(f'thank you {name}')
+        break
 
 #Exercise 7: Favorite Fruits
-# fav_fruits=input('enter your favorite fruits : ')
-# list_of_fruits=fav_fruits.split(' ')
-# print('list of fruits : ',list_of_fruits)
-# fruit=input('enter a fruit : ')
-# if fruit in list_of_fruits:
-#     print('you chose one of your favorite fruits! Enjoy!')
-# else:
-#     print('You chose a new fruit. I hope you enjoy it!')
+fav_fruits=input('enter your favorite fruits : ')
+list_of_fruits=fav_fruits.split(' ')
+print('list of fruits : ',list_of_fruits)
+fruit=input('enter a fruit : ')
+if fruit in list_of_fruits:
+    print('you chose one of your favorite fruits! Enjoy!')
+else:
+    print('You chose a new fruit. I hope you enjoy it!')
 
 #Exercise 8: Pizza Toppings
 # print('**** exercise 8 ****')
-# pizza_toppings=[]
-# topping=input('enter a topping : ')
-# while True:
-#     if topping =='quit':
-#         break
-#     else:
+pizza_toppings=[]
+topping=input('enter a topping : ')
+while True:
+    if topping =='quit':
+        break
+    else:
 
-#         pizza_toppings.append(topping)
-#         print(f'Adding {topping} to your pizza.')
-#         topping=input('enter a topping : ')
-#         continue
-# print('your pizza toppings are : ')
-# for topping in pizza_toppings :
-#     if topping !='quit':
-#         print(topping)
-#     else:
-#         break
-# print(f'total price is : {len(pizza_toppings)*1.5 + 10}$')
+        pizza_toppings.append(topping)
+        print(f'Adding {topping} to your pizza.')
+        topping=input('enter a topping : ')
+        continue
+print('your pizza toppings are : ')
+for topping in pizza_toppings :
+    if topping !='quit':
+        print(topping)
+    else:
+        break
+print(f'total price is : {len(pizza_toppings)*1.5 + 10}$')
 
 #Exercise 9: Cinemax Tickets
 print('**** exercise 9 ****')
-# ages=input('enter the ages for each member of the family (separated by spaces) : ')
-# list_of_ages=ages.split(' ')
-# total_price=0
-# for age in list_of_ages:
-#     if int(age)<3:
-#         total_price+=0
-#     elif int(age)>=3 and int(age)<12:
-#         total_price+=10
-#     else:
-#         total_price+=15
-# print(f'total price is : {total_price}$')
+ages=input('enter the ages for each member of the family (separated by spaces) : ')
+list_of_ages=ages.split(' ')
+total_price=0
+for age in list_of_ages:
+    if int(age)<3:
+        total_price+=0
+    elif int(age)>=3 and int(age)<12:
+        total_price+=10
+    else:
+        total_price+=15
+print(f'total price is : {total_price}$')
 
 #Bonus:
 print('**** bonus ****')
